c6ab50d96bbae1877db370e56ae194af2e5c3e71
vapvin <tpehdj12@daum.net>
2020-02-28 17:31:40 +0900

    Friend

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+private:
+    int studentId;
+    string name;
+public:
+    Student(int studentId, string name): studentId(studentId), name(name) { }
+    friend Student operator +(const Student &student, const Student &other){
+        return Student(student.studentId, student.name + "&" + other.name);
+    }
+    void showName() {
+        cout << "이름: " << name << "\n";
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Student student(1, "문현규");
+    Student result = student + student;
+    result.showName();
     return 0;
 }
